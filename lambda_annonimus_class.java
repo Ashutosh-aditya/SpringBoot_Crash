06133f60194a113248f13d607a3830e85eee1322
Ashutosh-aditya <adityaashutosh06@gmail.com>
2024-06-18 12:05:02 +0530

    added more information for  shortening the code with lambda expression

--- lambda_annonimus_class.java
+++ lambda_annonimus_class.java
@@ -11,6 +11,11 @@ interface MyFunctionalInterface {
     // A functional interface in Java is an interface that has exactly one abstract method. These interfaces are also known as Single Abstract Method (SAM) interfaces. They can have multiple default or static methods, but only one abstract method. Functional interfaces are used extensively in lambda expressions and method references in Java.
 }
 
+@FunctionalInterface
+interface MyFunctionalInterface2 {
+    int add(int a, int b);
+}
+
 
 public class lambda_annonimus_class {
     public static void main(String[] args) {
@@ -36,7 +41,15 @@ public class lambda_annonimus_class {
         MyFunctionalInterface obj3 = () -> System.out.println("Hello World 4" );
         obj3.execute();
 
-        
+        //code with return
+        MyFunctionalInterface2 obj4 = (i,j) -> {//no need to mention the datatype as it is already present in interface
+            return i+j;
+        };
+        System.out.println(obj4.add(5,7));
 
+        //further shorten the code
+        //code with return
+        MyFunctionalInterface2 obj5 = (i,j) ->  i+j;  //Single line fn no need to write return
+        System.out.println(obj5.add(10,20));
     }
 }
